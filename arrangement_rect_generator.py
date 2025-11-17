731a9dfb96bcdbfc1540cc77a0cd7bee9637636d
TokeHOlesen <tokeholesen@gmail.com>
2025-11-17 12:00:42 +0100

    Last refinements before added PDF drawing support

--- arrangement_rect_generator.py
+++ arrangement_rect_generator.py
@@ -1,16 +1,23 @@
-from pallet_rect_class import PalletRect
+from PyQt6.QtGui import QColor
+from PyQt6.QtCore import QRect
 import constants
 
-color = constants.PALLET_COLORS
+colors = constants.PALLET_COLORS
+
+
+class PalletRect(QRect):
+    def __init__(self, *args, color):
+        super().__init__(*args)
+        self.color = QColor(color)
 
 
 def gen_60_60_60(x, y, _, gutter, dimensions):
     p60_width = dimensions["standard_width"]
     p60_height = dimensions["v-60-height"]
 
-    pallet_rects = [PalletRect(x, y, p60_width, p60_height, color=color["60"]),
-                    PalletRect(x + p60_width + gutter, y, p60_width, p60_height, color=color["60"]),
-                    PalletRect(x + 2 * (p60_width + gutter), y, p60_width, p60_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, p60_width, p60_height, color=colors["60"]),
+                    PalletRect(x + p60_width + gutter, y, p60_width, p60_height, color=colors["60"]),
+                    PalletRect(x + 2 * (p60_width + gutter), y, p60_width, p60_height, color=colors["60"])]
 
     brush_offset = p60_height + gutter
     return pallet_rects, brush_offset
@@ -20,9 +27,9 @@ def gen_120_120_120(x, y, _, gutter, dimensions):
     p120_width = dimensions["standard_width"]
     p120_height = dimensions["v-120-height"]
 
-    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + p120_width + gutter, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + 2 * (p120_width + gutter), y, p120_width, p120_height, color=color["120"])]
+    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + p120_width + gutter, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + 2 * (p120_width + gutter), y, p120_width, p120_height, color=colors["120"])]
 
     brush_offset = p120_height + gutter
     return pallet_rects, brush_offset
@@ -31,10 +38,10 @@ def gen_120_120_120(x, y, _, gutter, dimensions):
 def gen_120_120(x, y, border_rect, gutter, dimensions):
     p120_width = dimensions["h-120-width"]
     p120_height = dimensions["h-120-height"]
-    p120_last_width = border_rect.width() - p120_width - gutter * 3
+    p120_adj_width = border_rect.width() - p120_width - gutter * 3
 
-    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + p120_width + gutter, y, p120_last_width, p120_height, color=color["120"])]
+    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + p120_width + gutter, y, p120_adj_width, p120_height, color=colors["120"])]
 
     brush_offset = p120_height + gutter
     return pallet_rects, brush_offset
@@ -44,12 +51,12 @@ def gen_120_120_60_60(x, y, _, gutter, dimensions):
     p120_width = dimensions["standard_width"]
     p120_height = dimensions["v-120-height"]
     p60_height = (p120_height - gutter) // 2
-    p60_last_height = p120_height - p60_height - gutter
+    p60_adj_height = p120_height - p60_height - gutter
 
-    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + p120_width + gutter, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + 2 * (p120_width + gutter), y, p120_width, p60_height, color=color["60"]),
-                    PalletRect(x + 2 * (p120_width + gutter), y + p60_height + gutter, p120_width, p60_last_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + p120_width + gutter, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + 2 * (p120_width + gutter), y, p120_width, p60_height, color=colors["60"]),
+                    PalletRect(x + 2 * (p120_width + gutter), y + p60_height + gutter, p120_width, p60_adj_height, color=colors["60"])]
 
     brush_offset = p120_height + gutter
     return pallet_rects, brush_offset
@@ -61,10 +68,10 @@ def gen_17080_17080_120_60(x, y, _, gutter, dimensions):
     p120_height = dimensions["v-120-height"]
     p60_height = p170_height - p120_height - gutter
 
-    pallet_rects = [PalletRect(x, y, pallet_width, p170_height, color=color["17080"]),
-                    PalletRect(x + pallet_width + gutter, y, pallet_width, p170_height, color=color["17080"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p120_height, color=color["120"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y + p120_height + gutter, pallet_width, p60_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, pallet_width, p170_height, color=colors["17080"]),
+                    PalletRect(x + pallet_width + gutter, y, pallet_width, p170_height, color=colors["17080"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p120_height, color=colors["120"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y + p120_height + gutter, pallet_width, p60_height, color=colors["60"])]
 
     brush_offset = p170_height + gutter
     return pallet_rects, brush_offset
@@ -76,11 +83,11 @@ def gen_17080_120_120_60_60(x, y, _, gutter, dimensions):
     p120_height = dimensions["v-120-height"]
     p60_height = p170_height - p120_height - gutter
 
-    pallet_rects = [PalletRect(x, y, pallet_width, p170_height, color=color["17080"]),
-                    PalletRect(x + pallet_width + gutter, y, pallet_width, p120_height, color=color["120"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p120_height, color=color["120"]),
-                    PalletRect(x + pallet_width + gutter, y + p120_height + gutter, pallet_width, p60_height, color=color["60"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y + p120_height + gutter, pallet_width, p60_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, pallet_width, p170_height, color=colors["17080"]),
+                    PalletRect(x + pallet_width + gutter, y, pallet_width, p120_height, color=colors["120"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p120_height, color=colors["120"]),
+                    PalletRect(x + pallet_width + gutter, y + p120_height + gutter, pallet_width, p60_height, color=colors["60"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y + p120_height + gutter, pallet_width, p60_height, color=colors["60"])]
 
     brush_offset = p170_height + gutter
     return pallet_rects, brush_offset
@@ -90,13 +97,13 @@ def gen_120_60_60_60_60(x, y, _, gutter, dimensions):
     pallet_width = dimensions["standard_width"]
     p120_height = dimensions["v-120-height"]
     p60_height = (p120_height - gutter) // 2
-    p60_last_height = p120_height - p60_height - gutter
+    p60_adj_height = p120_height - p60_height - gutter
 
-    pallet_rects = [PalletRect(x, y, pallet_width, p120_height, color=color["120"]),
-                    PalletRect(x + pallet_width + gutter, y, pallet_width, p60_height, color=color["60"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p60_height, color=color["60"]),
-                    PalletRect(x + pallet_width + gutter, y + p60_height + gutter, pallet_width, p60_last_height, color=color["60"]),
-                    PalletRect(x + 2 * (pallet_width + gutter), y + p60_height + gutter, pallet_width, p60_last_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, pallet_width, p120_height, color=colors["120"]),
+                    PalletRect(x + pallet_width + gutter, y, pallet_width, p60_height, color=colors["60"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y, pallet_width, p60_height, color=colors["60"]),
+                    PalletRect(x + pallet_width + gutter, y + p60_height + gutter, pallet_width, p60_adj_height, color=colors["60"]),
+                    PalletRect(x + 2 * (pallet_width + gutter), y + p60_height + gutter, pallet_width, p60_adj_height, color=colors["60"])]
 
     brush_offset = p120_height + gutter
     return pallet_rects, brush_offset
@@ -106,9 +113,9 @@ def gen_145_145_145(x, y, _, gutter, dimensions):
     p145_width = dimensions["standard_width"]
     p145_height = dimensions["v-145-height"]
 
-    pallet_rects = [PalletRect(x, y, p145_width, p145_height, color=color["145"]),
-                    PalletRect(x + p145_width + gutter, y, p145_width, p145_height, color=color["145"]),
-                    PalletRect(x + 2 * (p145_width + gutter), y, p145_width, p145_height, color=color["145"])]
+    pallet_rects = [PalletRect(x, y, p145_width, p145_height, color=colors["145"]),
+                    PalletRect(x + p145_width + gutter, y, p145_width, p145_height, color=colors["145"]),
+                    PalletRect(x + 2 * (p145_width + gutter), y, p145_width, p145_height, color=colors["145"])]
 
     brush_offset = p145_height + gutter
     return pallet_rects, brush_offset
@@ -118,9 +125,9 @@ def gen_17080_17080_17080(x, y, _, gutter, dimensions):
     p17080_width = dimensions["standard_width"]
     p17080_height = dimensions["v-17080-height"]
 
-    pallet_rects = [PalletRect(x, y, p17080_width, p17080_height, color=color["17080"]),
-                    PalletRect(x + p17080_width + gutter, y, p17080_width, p17080_height, color=color["17080"]),
-                    PalletRect(x + 2 * (p17080_width + gutter), y, p17080_width, p17080_height, color=color["17080"])]
+    pallet_rects = [PalletRect(x, y, p17080_width, p17080_height, color=colors["17080"]),
+                    PalletRect(x + p17080_width + gutter, y, p17080_width, p17080_height, color=colors["17080"]),
+                    PalletRect(x + 2 * (p17080_width + gutter), y, p17080_width, p17080_height, color=colors["17080"])]
 
     brush_offset = p17080_height + gutter
     return pallet_rects, brush_offset
@@ -132,9 +139,9 @@ def gen_17090_145_145(x, y, border_rect, gutter, dimensions):
     p145_width = border_rect.width() - p17090_width - gutter * 3
     p145_height = dimensions["h-145-height"]
 
-    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=color["17090"]),
-                    PalletRect(x + p17090_width + gutter, y, p145_width, p145_height, color=color["145"]),
-                    PalletRect(x + p17090_width + gutter, y + p145_height + gutter, p145_width, p145_height, color=color["145"])]
+    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=colors["17090"]),
+                    PalletRect(x + p17090_width + gutter, y, p145_width, p145_height, color=colors["145"]),
+                    PalletRect(x + p17090_width + gutter, y + p145_height + gutter, p145_width, p145_height, color=colors["145"])]
 
     brush_offset = p17090_height + gutter
     return pallet_rects, brush_offset
@@ -145,13 +152,13 @@ def gen_17090_17090_130_130_130(x, y, border_rect, gutter, dimensions):
     p17090_height = dimensions["v-17080-height"]
     p130_width = border_rect.width() - p17090_width - gutter * 3
     p130_height = int(round((p17090_height * 2 - gutter) / 3))
-    p130_last_height = (p17090_height * 2 + gutter) - (p130_height + gutter) * 2
+    p130_adj_height = (p17090_height * 2 + gutter) - (p130_height + gutter) * 2
 
-    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=color["17090"]),
-                    PalletRect(x, y + p17090_height + gutter, p17090_width, p17090_height, color=color["17090"]),
-                    PalletRect(x + p17090_width + gutter, y, p130_width, p130_height, color=color["130"]),
-                    PalletRect(x + p17090_width + gutter, y + p130_height + gutter, p130_width, p130_height, color=color["130"]),
-                    PalletRect(x + p17090_width + gutter, y + (p130_height + gutter) * 2, p130_width, p130_last_height, color=color["130"])]
+    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=colors["17090"]),
+                    PalletRect(x, y + p17090_height + gutter, p17090_width, p17090_height, color=colors["17090"]),
+                    PalletRect(x + p17090_width + gutter, y, p130_width, p130_height, color=colors["130"]),
+                    PalletRect(x + p17090_width + gutter, y + p130_height + gutter, p130_width, p130_height, color=colors["130"]),
+                    PalletRect(x + p17090_width + gutter, y + (p130_height + gutter) * 2, p130_width, p130_adj_height, color=colors["130"])]
 
     brush_offset = (p17090_height + gutter) * 2
     return pallet_rects, brush_offset
@@ -163,9 +170,9 @@ def gen_130_120_120(x, y, border_rect, gutter, dimensions):
     p120_width = border_rect.width() - p130_width - gutter * 3
     p120_height = dimensions["h-120-height"]
 
-    pallet_rects = [PalletRect(x, y, p130_width, p130_height, color=color["130"]),
-                    PalletRect(x + p130_width + gutter, y, p120_width, p120_height, color=color["120"]),
-                    PalletRect(x + p130_width + gutter, y + p120_height + gutter, p120_width, p120_height, color=color["120"])]
+    pallet_rects = [PalletRect(x, y, p130_width, p130_height, color=colors["130"]),
+                    PalletRect(x + p130_width + gutter, y, p120_width, p120_height, color=colors["120"]),
+                    PalletRect(x + p130_width + gutter, y + p120_height + gutter, p120_width, p120_height, color=colors["120"])]
 
     brush_offset = (p120_height * 2) + (gutter * 2)
     return pallet_rects, brush_offset
@@ -176,8 +183,8 @@ def gen_130_130(x, y, border_rect, gutter, dimensions):
     p130_height = dimensions["v-130-height"]
     p130_last_width = border_rect.width() - p130_width - gutter * 3
 
-    pallet_rects = [PalletRect(x, y, p130_width, p130_height, color=color["130"]),
-                    PalletRect(x + p130_width + gutter, y, p130_last_width, p130_height, color=color["130"])]
+    pallet_rects = [PalletRect(x, y, p130_width, p130_height, color=colors["130"]),
+                    PalletRect(x + p130_width + gutter, y, p130_last_width, p130_height, color=colors["130"])]
 
     brush_offset = p130_height + gutter
     return pallet_rects, brush_offset
@@ -188,8 +195,8 @@ def gen_17080_60(x, y, border_rect, gutter, dimensions):
     p60_width = border_rect.width() - p17080_width - gutter * 3
     pallet_height = dimensions["h-17080-height"]
 
-    pallet_rects = [PalletRect(x, y, p17080_width, pallet_height, color=color["17080"]),
-                    PalletRect(x + p17080_width + gutter, y, p60_width, pallet_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, p17080_width, pallet_height, color=colors["17080"]),
+                    PalletRect(x + p17080_width + gutter, y, p60_width, pallet_height, color=colors["60"])]
 
     brush_offset = pallet_height + gutter
     return pallet_rects, brush_offset
@@ -201,8 +208,8 @@ def gen_17090_60(x, y, border_rect, gutter, dimensions):
     p60_width = border_rect.width() - p17090_width - gutter * 3
     p60_height = dimensions["h-60-height"]
 
-    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=color["17090"]),
-                    PalletRect(x + p17090_width + gutter, y, p60_width, p60_height, color=color["60"])]
+    pallet_rects = [PalletRect(x, y, p17090_width, p17090_height, color=colors["17090"]),
+                    PalletRect(x + p17090_width + gutter, y, p60_width, p60_height, color=colors["60"])]
 
     brush_offset = p17090_height + gutter
     return pallet_rects, brush_offset
@@ -212,7 +219,7 @@ def gen_23090(x, y, _, gutter, dimensions):
     pallet_width_px = dimensions["h-23090-width"]
     pallet_height_px = dimensions["h-23090-height"]
 
-    pallet_rects = [PalletRect(x, y, pallet_width_px, pallet_height_px, color=color["23090"])]
+    pallet_rects = [PalletRect(x, y, pallet_width_px, pallet_height_px, color=colors["23090"])]
 
     brush_offset = pallet_height_px + gutter
     return pallet_rects, brush_offset
--- pallet_rect_class.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtGui import QColor
-from PyQt6.QtCore import QRect
-
-
-class PalletRect(QRect):
-    def __init__(self, *args, color=QColor("white")):
-        super().__init__(*args)
-        self.color = QColor(color)
